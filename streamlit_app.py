7d99cbae3eb13931f2212e10c22c7dad8c5e7caf
fadili101 <55854031+fadili101@users.noreply.github.com>
2024-12-13 22:29:22 +0000

    test31

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,31 @@
 import streamlit as st
 import cv2
 import imutils
+import numpy as np
+from PIL import Image
+import io
 
+# Active la caméra si la case est cochée
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
+    # Afficher l'image capturée
     st.image(picture)
-#image = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
-image = cv2.imread(picture)
 
-# resize it to have a maximum width of 400 pixels
-image = imutils.resize(image, width=400)
-(h, w) = image.shape[:2]
-print("[INFO] loading model...")
-prototxt = 'deploy.prototxt'
-model = 'res10_300x300_ssd_iter_140000.caffemodel'
-net = cv2.dnn.readNetFromCaffe(prototxt, model)
+    # Convertir l'image Streamlit en un format OpenCV (numpy array)
+    image = Image.open(io.BytesIO(picture))  # Charger l'image depuis les octets
+    image = np.array(image)  # Convertir en numpy array
+
+    # Redimensionner l'image pour avoir une largeur maximale de 400 pixels
+    image = imutils.resize(image, width=400)
+    (h, w) = image.shape[:2]
+    print("[INFO] loading model...")
+
+    # Charger le modèle de détection d'objets
+    prototxt = 'deploy.prototxt'
+    model = 'res10_300x300_ssd_iter_140000.caffemodel'
+    net = cv2.dnn.readNetFromCaffe(prototxt, model)
+
+    # Afficher les dimensions de l'image
+    print(f"Image dimensions: {h}x{w}")

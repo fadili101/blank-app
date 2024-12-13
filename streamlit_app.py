704fe81c15e386872d27dbc87110ac6864ff283d
fadili101 <55854031+fadili101@users.noreply.github.com>
2024-12-13 22:20:22 +0000

    test20

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import cv2
 
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
     st.image(picture)
+#image = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
+image = cv2.imread(picture)
+
+# resize it to have a maximum width of 400 pixels
+image = imutils.resize(image, width=400)
+(h, w) = image.shape[:2]
+print("[INFO] loading model...")
+prototxt = 'deploy.prototxt'
+model = 'res10_300x300_ssd_iter_140000.caffemodel'
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
